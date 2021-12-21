35826d7fbad0231ac78042c37504f5efce8fdafb
XMTongXie <64581233+XMTongXie@users.noreply.github.com>
2021-12-21 11:31:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
  
 header = {
-    'Cookie': '*********************',
+    'Cookie': '*******添加用户cookie**************',
     'Upgrade-Insecure-Requests': '1',
-    'User-Agent': '******************',
+    'User-Agent': '***添加用户Agent***************',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
     'Accept-Encoding': 'gzip, deflate',
